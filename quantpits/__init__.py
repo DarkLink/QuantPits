a21c54894992a3da1934a7fcdc2b8c9cef63d0f0
Tonny@Home <homepc@anatoray.com>
2026-05-17 23:07:29 +0800

    chore(release): bump version to v0.4.2-alpha

--- quantpits/__init__.py
+++ quantpits/__init__.py
@@ -6,4 +6,4 @@ try:
     __version__ = version("quantpits")
 except PackageNotFoundError:
     # Package is not installed (running from source without pip install -e .)
-    __version__ = "0.4.1-alpha"
+    __version__ = "0.4.2-alpha"
